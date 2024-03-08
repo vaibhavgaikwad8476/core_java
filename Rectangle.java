31d7bb69dc09007264acb31ba1a22ea2432c92a0
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 23:49:34 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle
+{
+public static void main(String [] args)
+{
+double width = 4.5;
+double height = 7.9;
+double area = width*height;
+double parameter = 2*(width + height);
+System.out.println("Area Of Rectangle: "+area);
+System.out.println("Parameter Of Rectangle: "+parameter);
+}
+}
