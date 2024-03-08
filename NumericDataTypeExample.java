5184306778e1153fb6063f3ef39b09179432c40f
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 23:56:45 +0530

    Add files via upload

---
+++ NumericDataTypeExample.java
@@ -0,0 +1,23 @@
+class NumericDataTypeExample {
+    public static void main(String[] args) 
+    {    
+    byte byteVar = 100;
+    short shortVar = 1000;
+    int intVar = 10000;
+    long longVar = 100000L;
+        
+      byte byteVar2 = -50;
+      short shortVar2 = -1000;
+      int intVar2 = -10000;
+      long longVar2 = -100000L;
+        
+        System.out.println("byteVar: " + byteVar);
+        System.out.println("shortVar: " + shortVar);
+        System.out.println("intVar: " + intVar);
+        System.out.println("longVar: " + longVar);
+        System.out.println("byteVar2: " + byteVar2);
+        System.out.println("shortVar2: " + shortVar2);
+        System.out.println("intVar2: " + intVar2);
+        System.out.println("longVar2: " + longVar2);
+    }
+}
