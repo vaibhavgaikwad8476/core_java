e33855fefd3841365cac7d8e2aaaaa3a1a6c5f55
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 23:58:29 +0530

    Add files via upload

---
+++ FloatExample.java
@@ -0,0 +1,16 @@
+class FloatExample {
+    public static void main(String[] args) {
+        float num1 = 10.5f;
+        float num2 = 20.7f;
+  
+
+        float sum = num1 + num2;
+        float difference = num1 - num2;
+
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+
+    }
+}
