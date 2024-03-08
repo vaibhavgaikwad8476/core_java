14ae835485bc8e220fff60409b2e0142998ccd71
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 22:37:51 +0530

    Add files via upload

---
+++ FinancialApplication.java
@@ -0,0 +1,12 @@
+import java.util.java.Scanner;
+Class FinancialApplication
+{
+    public static void main (String [] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter The Subtotal ");
+      
+
+    }
+
+}
