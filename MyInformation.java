8b5c80a0556b4a9f2bb98733803782dfeb4853a7
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 23:54:03 +0530

    Add files via upload

---
+++ MyInformation.java
@@ -0,0 +1,8 @@
+class MyInformation {
+    public static void main(String[] args) {
+        System.out.println("Name: Vaibhav Gaikwad");
+        System.out.println("Native: Karjat");
+        System.out.println("Degree;: BE ");
+        System.out.println("YOP: 2024");
+    }
+}
