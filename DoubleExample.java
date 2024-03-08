57f14179639735687a1d21c7fd333bb14cd3d793
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 22:36:13 +0530

    Add files via upload

---
+++ DoubleExample.java
@@ -0,0 +1,16 @@
+ class DoubleExample {
+    public static void main(String[] args) {
+       
+        double num1 = 10.5;
+        double num2 = 20.7;
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+
+    }
+}
