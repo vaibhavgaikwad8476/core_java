0b74d1ce2936ca6e6c359f8c159c4dc24e74dbec
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-08 22:33:02 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+// write a java program to find a area of circle whose radius 2.5cm 
+
+class AreaOfCircle
+{
+public static void main(String [] args)
+{
+double radius = 5.5;
+final double pi = 3.14;
+double area = pi * (radius*radius);
+System.out.println("Area Of Circle is: "+area+"cm^2");
+double parameter = 2 * pi * radius;
+System.out.println("Parameter is: "+parameter);
+}
+}
