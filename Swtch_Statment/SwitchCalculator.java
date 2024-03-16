9f2fd596a1781667ab7f81dc6e678acd65ee393f
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-16 14:14:48 +0530

    Add files via upload

---
+++ Swtch_Statment/SwitchCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class SwitchCalculator
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number1 : ");
+		double num1 = sc.nextDouble();
+		System.out.print("Enter a operator: ");
+		char op = sc.next().charAt(0);
+		System.out.print("Enter a number2 : ");
+		double num2 = sc.nextDouble();
+
+		boolean flag = true;
+		double output = 0;
+
+		switch(op)
+		{
+		case '+' : output = num1 + num2; break;
+		case '-' : output = num1 - num2; break;
+		case '*' : output = num1 * num2; break;
+		case '/' : output = num1 / num2; break;
+		case '%' : output = num1 % num2; break;
+		default : System.out.println("Invalid output");flag = false;
+
+		}
+		if(flag)
+		{
+ 			System.out.println(num1 + " "+op+ " "+ num2+ "="+output);
+		}
+	}
+}
