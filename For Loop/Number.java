caa7997ab69413febc43db8d8705f16532a2a296
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-18 20:19:53 +0530

    Add files via upload

---
+++ For Loop/Number.java
@@ -0,0 +1,10 @@
+class Number
+{
+		public static void main(String[] args) {
+			System.out.println("1 To 100 Numbers: ");
+				for(int i = 1; i<=100;i++)
+				{
+						System.out.print(i+ "  ");
+				}
+		}
+}
