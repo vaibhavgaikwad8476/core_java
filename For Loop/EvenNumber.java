346e533183814d1fe829085c29550d5f805257e1
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-03-18 20:18:26 +0530

    Add files via upload

---
+++ For Loop/EvenNumber.java
@@ -0,0 +1,13 @@
+class EvenNumber
+{
+		public static void main(String[] args) {
+			System.out.println("Even Number 1 To 100: ");
+			for(int i = 1;i<= 100;i++)
+				{
+					if(i %2==0)
+					System.out.print(i+ "  ");
+				}
+
+		}
+
+}
