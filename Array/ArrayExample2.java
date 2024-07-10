16b4e5252a8b0750d796611ac6ff040521a83dfd
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-07-10 23:01:04 +0530

    Add files via upload

---
+++ Array/ArrayExample2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class ArrayExample2
+{
+	public static void main(String[] args) {
+		int [][][] a= new int [2][][];
+		a[0] = new int [1][2];
+		a[1] = new int [1][4];
+
+		a[0][0][]
+	}
+}
